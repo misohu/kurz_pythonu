7c3578e4b0743e028590764f069741f15b103619
Hucko <Michal_Hucko@Dell.com>
2020-10-13 10:32:42 +0200

    Dev: add bools examples.

---
+++ 003_bools.py
@@ -0,0 +1,23 @@
+true_value = True
+false_value = False
+
+3 < 4 
+5 > 6
+5 == 7 
+4 <= 8
+8 == 8.0
+9 != 10
+
+x=True
+not(x)
+
+3 < 4 and 5 > 6
+3 < 4 or 5 > 6
+not(3 < 4 and 5 > 6)
+
+# Cvicenie na doma skus uhadnut pravdivostnu hodnotu potom to over v pythone
+not(4 <= 16) and 5 > 7 
+17 != 14 or 4 > 6
+9 == 8 and 3 <= 2
+10 >= 13 and 4 != 3 or 12 < 14
+not(3 > 6) or not(4 == 18 or 7 > 8) 
