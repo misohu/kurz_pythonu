a2bff31274586aa31bef9949f6d37414a917e77e
Hucko <Michal_Hucko@Dell.com>
2020-10-13 11:08:12 +0200

    Dev: add strings lecture.

---
+++ 004_strings.py
@@ -0,0 +1,31 @@
+string="This is string"
+print(string)
+string='This is string with single quotes'
+print(string)
+string='This is single quote \' in single quote string'
+print(string)
+string='This is double quote " in single quote'
+print(string)
+string="This is single quote ' in double quote"
+print(string)
+string="This is string with \nnew line"
+print(string)
+string="This is string with \n\tnew line and tab"
+print(string)
+
+tricky_string_path="C:\documents\notes"
+print(tricky_string_path)
+tricky_string_path_fixed=r"C:\documents\notes"
+print(tricky_string_path_fixed)
+string="""
+This is  
+multiline 
+string.
+"""
+print(string)
+string="""\
+This is  
+multiline 
+string. Empty lines fixed\
+"""
+print(string)
