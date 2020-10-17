450039230467b76981f2007ea6ae9f2909863a36
Michal Hucko <michalhucko@Michals-MacBook-Pro.local>
2020-10-17 12:54:31 +0200

    Dev: add while cycle code

---
+++ 010_while_cycle.py
@@ -0,0 +1,40 @@
+# While cyklus ma v sebe podmienku a funguje podobne ako if
+counter=0
+while counter< 5:
+    print(counter)
+    counter+=1
+
+# Nekonecny cyklus vieme dosiahnut pomocou while True
+counter = 0
+while True:
+    print(counter)
+
+# Nezabudnite cyklus ukoncit inac pojde do nekonecna.
+# Ukoncit cyklus vieme pomocou kluceoveho slova break
+counter = 0
+while True: 
+    print(counter) 
+    if counter >= 5:
+        break
+    counter += 1
+
+# Break vieme pouzit aj na ukoncenie for cyklu
+for i in [1,2,3,4]:
+    print(i)
+    if i == 2:
+        break
+
+# Klucove slove continue sa pouziva na predcasne ukoncenie iteracie nie vsak celeho cyklu
+counter = 0
+while True:  
+    if counter == 2:
+        continue
+    print(counter)
+    counter += 1
+
+# Opat ho vieme pouzit vo for cikle
+for i in [1,2,3,4]:
+    if i == 2:
+        continue
+    print(i)
+
