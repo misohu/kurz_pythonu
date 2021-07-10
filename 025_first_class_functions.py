60f88d7b10b7766291673ba1d0df71f6180ea68b
Michal Hucko <michalhucko@Michals-MacBook-Pro.local>
2021-07-10 13:24:47 +0200

    Dev: add decorator source code

---
+++ 025_first_class_functions.py
@@ -0,0 +1,50 @@
+# Funkcie v Pythone sú takzvané funkcia prvej triedy 'First class functions'
+def hello_world():
+    print("helllo world")
+
+print(type(hello_world)) # Aj funkcia je len objekt (pre tych co videli moj kurz OOP)
+
+# To znamená že ich napríklad vieme priradiť do premenných
+x=hello_world # vsimni si ze nepouzivam okruhle zatvorky to znamena ze nevolam funkciu
+x()
+
+# Ale aj poslat ako parameter funkcie
+def function_wrapper(func):
+    print("This is function wrapper")
+    func()
+
+function_wrapper(hello_world)
+
+# Funkcia tiez vie vratit funkciu, ktora sa v nej vytvori
+def function_factory():
+    def hello_world():
+        print("Hello world but different")
+    return hello_world
+
+print(function_factory)
+print(function_factory())
+print(function_factory()())
+
+# Funkcie vies ulozit do pola presne ako akekolvek ine hodnoty
+def adder(x, y):
+    return x + y
+
+def substractor(x, y):
+    return x - y
+
+def multiplier(x, y):
+    return x * y
+
+math_functions = [adder, substractor, multiplier]
+
+for function in math_functions:
+    print(function(1, 2))
+
+list(map(lambda func: print(func(1,2)), math_functions))
+
+# Annonymous function
+def function_wrapper(func, x, y):
+    print("This is function wrapper")
+    return func(x, y)
+
+print(function_wrapper(lambda x,y: x+y, 10, 20))
---
+++ 026_decorator.py
@@ -0,0 +1,120 @@
+visitor={
+    "name": "Michal",
+    "age": 13
+}
+
+def play_movie(visitor):
+    print(f"I am playing movie for {visitor['name']}")
+
+# What is we want to play movie only for adults
+if visitor['age'] >= 18:
+    play_movie(visitor)
+
+# Decorator
+def play_movie():
+    print(f"I am playing movie")
+
+def adult_function(func):
+    def make_adult_function():
+        if visitor['age'] >= 18:
+            return func()
+        else:
+            print("Grow up!!!")
+    return make_adult_function
+
+play_movie=adult_function(play_movie)
+play_movie()
+
+# Decorator @
+@adult_function
+def show_image():
+    print("Showing image")
+
+show_image()
+
+
+# Decorators with arguments
+# niekedy chceme aby nasa funcktia mala viacero argumentov 
+import datetime
+import time
+
+def log_duration(func):
+    def wrapper(x):
+        print(datetime.datetime.now())
+        func(x)
+        print(datetime.datetime.now())
+    return wrapper
+
+@log_duration
+def sleeper(seconds):
+    time.sleep(seconds)
+
+# sleeper(5)
+
+# Co ak chcem viacero argumentov
+def log_duration(func):
+    def wrapper(*args):
+        print(datetime.datetime.now())
+        func(*args)
+        print(datetime.datetime.now())
+    return wrapper
+
+@log_duration
+def sleeper(seconds, name):
+    time.sleep(seconds)
+    print(f"Hi, my name is {name}")
+
+# sleeper(5, "Michal")
+
+
+# Co ak chceme nieco vratit
+def log_duration(func):
+    def wrapper(*args):
+        print(datetime.datetime.now())
+        return_value = func(*args)
+        print(datetime.datetime.now())
+        return return_value
+    return wrapper
+
+@log_duration
+def sleeper(seconds, name):
+    time.sleep(seconds)
+    print(f"Hi, my name is {name}")
+    return name
+
+# print(sleeper(5, "Michal"))
+
+
+# Introspection
+# Kazda funkcia si so sebou nesie meno a po dekorovani sa meni meno funkce 
+def hello_world():
+    print("Hello world")
+
+print(hello_world.__name__)
+
+@log_duration
+def hello_world():
+    print("Hello world")
+
+print(hello_world.__name__)
+
+# Takto to vieme opravit
+import functools
+
+def log_date(func):
+    @functools.wraps(func)
+    def wrapper(*args):
+        print(datetime.datetime.now)
+        return_value=func(*args)
+        print(datetime.datetime.now)
+        return return_value
+    return wrapper
+
+@log_date
+def hello_world():
+    print("Hello world")
+
+print(hello_world.__name__)
+
+# Domaca uloha spravte dekorator ktory zopakuje volanie funkcie 3 krat
+
